19274e31142463152c38297ab8fc5cf333615d86
Tim Eulitz <tim.eulitz@wikimedia.de>
2018-10-24 12:15:45 +0200

    Update controller

--- src/Controller/BannerSelectionController.php
+++ src/Controller/BannerSelectionController.php
@@ -6,26 +6,27 @@ namespace WMDE\BannerServer\Controller;
 
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
-use WMDE\BannerServer\UseCase\ProvideBannerValues;
-use WMDE\BannerServer\UseCase\ProvideBannerUseCase;
+use WMDE\BannerServer\UseCase\BannerSelection\BannerSelectionUseCase;
+use WMDE\BannerServer\UseCase\BannerSelection\Visitor;
 
 /**
  * @license GNU GPL v2+
  */
-class BannerController {
+class BannerSelectionController {
 
 	public const IMPRESSION_COUNT_COOKIE = 'impCount';
 	public const BANNER_IMPRESSION_COUNT_COOKIE = 'bImpCount';
 	public const BUCKET_COOKIE = 'b';
 	public const CAMPAIGN_COOKIE = 'cmp';
+	public const DONATED_COOKIE = 'd';
 
 	private $useCase;
 
-	public function __construct( ProvideBannerUseCase $useCase ) {
+	public function __construct( BannerSelectionUseCase $useCase ) {
 		$this->useCase = $useCase;
 	}
 
-	public function provideBanner( Request $request ): Response {
+	public function selectBanner( Request $request ): Response {
 		$bannerResponseData = $this->useCase->provideBannerRequest( $this->buildValuesFromRequest( $request ) );
 		if ( !$bannerResponseData->displayBanner() ) {
 			return new Response( '', Response::HTTP_OK );
@@ -33,12 +34,11 @@ class BannerController {
 		return new Response( 'Placeholder', Response::HTTP_OK );
 	}
 
-	private function buildValuesFromRequest( Request $request ): ProvideBannerValues {
-		return new ProvideBannerValues(
+	private function buildValuesFromRequest( Request $request ): Visitor {
+		return new Visitor(
 			$request->cookies->getInt( self::IMPRESSION_COUNT_COOKIE, 0 ),
-			$request->cookies->getInt( self::BANNER_IMPRESSION_COUNT_COOKIE, 0 ),
 			$request->cookies->get( self::BUCKET_COOKIE, null ),
-			$request->cookies->get( self::CAMPAIGN_COOKIE, null )
+			$request->cookies->get( self::DONATED_COOKIE, null )
 		);
 	}
 }
