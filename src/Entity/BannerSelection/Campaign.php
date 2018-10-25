1608d1f67e221a7fbaeb16d8c23cef5351178d90
tzhelyazkova <tonina.zhelyazkova@wikimedia.de>
2018-10-25 13:36:41 +0200

    Remove threshold reached logic from Campaign class

--- src/Entity/BannerSelection/Campaign.php
+++ src/Entity/BannerSelection/Campaign.php
@@ -12,19 +12,17 @@ class Campaign {
 	private $identifier;
 	private $start;
 	private $end;
-	private $impressionThreshold;
 
 	/**
 	 * @var Bucket[]
 	 */
 	private $buckets;
 
-	public function __construct( string $identifier, \DateTime $start, \DateTime $end, array $buckets, ImpressionThresholdInterface $impressionThreshold ) {
+	public function __construct( string $identifier, \DateTime $start, \DateTime $end, array $buckets ) {
 		$this->identifier = $identifier;
 		$this->start = $start;
 		$this->end = $end;
 		$this->buckets = $buckets;
-		$this->impressionThreshold = $impressionThreshold;
 	}
 
 	public function getIdentifier(): string {
@@ -43,9 +41,5 @@ class Campaign {
 			}
 		}
 		return $fallbackSelectionStrategy( $this->buckets );
-	}
-
-	public function impressionThresholdReached( int $visitorImpressions ): bool {
-		return $this->impressionThreshold->getIsOverThreshold( $visitorImpressions );
 	}
 }
