f11d99a6f15dcd918d4c952189305a1c08188915
Tim Eulitz <tim.eulitz@wikimedia.de>
2018-10-25 12:12:54 +0200

    Implement main parts of banner selection use case

--- src/UseCase/BannerSelection/BannerSelectionUseCase.php
+++ src/UseCase/BannerSelection/BannerSelectionUseCase.php
@@ -4,6 +4,8 @@ declare( strict_types = 1 );
 
 namespace WMDE\BannerServer\UseCase\BannerSelection;
 
+use WMDE\BannerServer\Entity\BannerSelection\Bucket;
+use WMDE\BannerServer\Entity\BannerSelection\Campaign;
 use WMDE\BannerServer\Entity\BannerSelection\CampaignCollection;
 
 /**
@@ -12,21 +14,39 @@ use WMDE\BannerServer\Entity\BannerSelection\CampaignCollection;
 class BannerSelectionUseCase {
 
 	private $campaignCollection;
+	private $currentCampaign;
 
 	public function __construct( CampaignCollection $campaignCollection ) {
 		$this->campaignCollection = $campaignCollection;
 	}
 
 	public function provideBannerRequest( Visitor $visitor ): BannerSelection {
-		if ( $visitor->hasDonated() ) {
+		if ( $visitor->hasDonated() ||
+			$this->getCurrentCampaign() === null ||
+			$this->getCurrentCampaign()->impressionThresholdReached( $visitor->getTotalImpressionCount() ) ) {
 			return BannerSelection::createEmptySelection( $visitor );
 		}
 
+		$visitorBucket = $this->getCurrentCampaign()->selectBucket(
+			$visitor->getBucketIdentifier(),
+			[ self::class, 'selectRandomBucket' ]
+		);
+
 		return BannerSelection::createBannerSelection(
-			'test',
-			new Visitor( 1, 'Testbucket', false ),
+			$visitorBucket->getBanner( $visitor->getTotalImpressionCount() ),
+			new Visitor( 1, $visitorBucket->getIdentifier(), false ),
 			new \DateTime()
 		);
 	}
 
+	private function getCurrentCampaign(): ?Campaign {
+		if ( $this->currentCampaign === null ) {
+			$this->currentCampaign = $this->campaignCollection->getCampaign( new \DateTime() );
+		}
+		return $this->currentCampaign;
+	}
+
+	public static function selectRandomBucket( Bucket ...$buckets ): Bucket {
+		return $buckets[random_int( 0, count( $buckets ) )];
+	}
 }
