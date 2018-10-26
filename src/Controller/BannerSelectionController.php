3f3e138c6bed2acc5b95a1a4bdf9ffe951ad5e17
timEulitz <tim.eulitz@wikimedia.de>
2018-10-26 15:48:10 +0200

    Implement missing Controller logic

--- src/Controller/BannerSelectionController.php
+++ src/Controller/BannerSelectionController.php
@@ -4,8 +4,11 @@ declare( strict_types = 1 );
 
 namespace WMDE\BannerServer\Controller;
 
+use Symfony\Component\HttpFoundation\Cookie;
+use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use WMDE\BannerServer\UseCase\BannerSelection\BannerSelection;
 use WMDE\BannerServer\UseCase\BannerSelection\BannerSelectionUseCase;
 use WMDE\BannerServer\UseCase\BannerSelection\Visitor;
 
@@ -15,9 +18,7 @@ use WMDE\BannerServer\UseCase\BannerSelection\Visitor;
 class BannerSelectionController {
 
 	public const IMPRESSION_COUNT_COOKIE = 'impCount';
-	public const BANNER_IMPRESSION_COUNT_COOKIE = 'bImpCount';
 	public const BUCKET_COOKIE = 'b';
-	public const CAMPAIGN_COOKIE = 'cmp';
 	public const DONATED_COOKIE = 'd';
 
 	private $useCase;
@@ -31,14 +32,42 @@ class BannerSelectionController {
 		if ( !$bannerResponseData->displayBanner() ) {
 			return new Response( '', Response::HTTP_OK );
 		}
-		return new Response( 'Placeholder', Response::HTTP_OK );
+
+		$response = new RedirectResponse(
+			$this->getBannerUrl( $bannerResponseData->getBannerIdentifier() ),
+			Response::HTTP_FOUND
+		);
+
+		foreach ( $this->getCookies(
+			$bannerResponseData->getVisitorData(),
+			$bannerResponseData->getCampaignEnd()->modify( '+2 week' )
+		) as $cookie ) {
+			$response->headers->setCookie( $cookie );
+		}
+
+		return $response;
 	}
 
 	private function buildValuesFromRequest( Request $request ): Visitor {
 		return new Visitor(
 			$request->cookies->getInt( self::IMPRESSION_COUNT_COOKIE, 0 ),
 			$request->cookies->get( self::BUCKET_COOKIE, null ),
-			$request->cookies->get( self::DONATED_COOKIE, null )
+			$request->cookies->get( self::DONATED_COOKIE, false )
 		);
+	}
+
+	private function getBannerUrl( string $bannerIdentifier ): string {
+		return 'content/' . $bannerIdentifier . '.js';
+	}
+
+	private function getCookies( Visitor $visitor, \DateTime $cookieExpirationDate ): array {
+		return [
+			new Cookie( self::BUCKET_COOKIE, $visitor->getBucketIdentifier(), $cookieExpirationDate ),
+			new Cookie(
+				self::IMPRESSION_COUNT_COOKIE,
+				(string)$visitor->getTotalImpressionCount(),
+				$cookieExpirationDate
+			)
+		];
 	}
 }
