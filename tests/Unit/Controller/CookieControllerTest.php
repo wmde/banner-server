b3a9e82ac1b5ce0159cc37993eedc7d37cafe604
Abban Dunne <abban.dunne@wikimedia.de>
2020-03-10 16:14:29 +0100

    Refactoring DonationFinishedControllerTest to test AbstractCookieController

--- tests/Unit/Controller/CookieControllerTest.php
+++ tests/Unit/Controller/CookieControllerTest.php
@@ -7,13 +7,17 @@ namespace WMDE\BannerServer\Tests\Unit\Controller;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\ResponseHeaderBag;
-use WMDE\BannerServer\Controller\DonationFinishedController;
+use WMDE\BannerServer\Controller\AbstractCookieController;
 use PHPUnit\Framework\TestCase;
 
-class DonationFinishedControllerTest extends TestCase {
+class MockCookieController extends AbstractCookieController {
+
+}
+
+class CookieControllerTest extends TestCase {
 
 	public function test_given_no_category_then_no_cookie_is_set(): void {
-		$controller = new DonationFinishedController( 'P1D' );
+		$controller = new MockCookieController( 'P1D' );
 
 		$response = $controller->index( new Request() );
 
@@ -22,7 +26,7 @@ class DonationFinishedControllerTest extends TestCase {
 	}
 
 	public function test_given_invalid_category_then_no_cookie_is_set(): void {
-		$controller = new DonationFinishedController( 'P1D' );
+		$controller = new MockCookieController( 'P1D' );
 
 		$response = $controller->index( new Request( ['c' => ':break-my-yaml!'] ) );
 
@@ -31,7 +35,7 @@ class DonationFinishedControllerTest extends TestCase {
 	}
 
 	public function test_given_a_category_then_category_cookie_is_set(): void {
-		$controller = new DonationFinishedController( 'P180D' );
+		$controller = new MockCookieController( 'P180D' );
 
 		$response = $controller->index( new Request( ['c' => 'fundraising,fundraising_next'] ) );
 
@@ -45,7 +49,7 @@ class DonationFinishedControllerTest extends TestCase {
 	}
 
 	public function test_given_image_content_type_zero_byte_image_is_returned(): void {
-		$controller = new DonationFinishedController( 'P180D' );
+		$controller = new MockCookieController( 'P180D' );
 
 		$request = new Request( ['c' => 'fundraising,fundraising_next'] );
 		$request->headers->set( 'accept', 'image/gif,image/png,image/*' );
