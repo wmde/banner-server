e0f7765bf9969ccf34bb41af0fa826a92a104963
Abban Dunne <Abban@users.noreply.github.com>
2020-03-11 17:21:19 +0100

    Add Cookie with Timout for Banner Closing (#39)
    Move cookie setting functionality into an abstract controller and inherit from it for concrete implementations with different cookie lifetimes.
    
    https://phabricator.wikimedia.org/T243614

---
+++ src/Controller/BannerClosedController.php
@@ -0,0 +1,9 @@
+<?php
+
+declare( strict_types = 1 );
+
+namespace WMDE\BannerServer\Controller;
+
+class BannerClosedController extends AbstractCookieController {
+
+}
--- tests/Unit/Controller/CookieControllerTest.php
+++ tests/Unit/Controller/CookieControllerTest.php
@@ -7,13 +7,13 @@ namespace WMDE\BannerServer\Tests\Unit\Controller;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\ResponseHeaderBag;
-use WMDE\BannerServer\Controller\DonationFinishedController;
 use PHPUnit\Framework\TestCase;
+use WMDE\BannerServer\Tests\Utils\TestCookieController;
 
-class DonationFinishedControllerTest extends TestCase {
+class CookieControllerTest extends TestCase {
 
 	public function test_given_no_category_then_no_cookie_is_set(): void {
-		$controller = new DonationFinishedController( 'P1D' );
+		$controller = new TestCookieController( 'P1D' );
 
 		$response = $controller->index( new Request() );
 
@@ -22,7 +22,7 @@ class DonationFinishedControllerTest extends TestCase {
 	}
 
 	public function test_given_invalid_category_then_no_cookie_is_set(): void {
-		$controller = new DonationFinishedController( 'P1D' );
+		$controller = new TestCookieController( 'P1D' );
 
 		$response = $controller->index( new Request( ['c' => ':break-my-yaml!'] ) );
 
@@ -31,7 +31,7 @@ class DonationFinishedControllerTest extends TestCase {
 	}
 
 	public function test_given_a_category_then_category_cookie_is_set(): void {
-		$controller = new DonationFinishedController( 'P180D' );
+		$controller = new TestCookieController( 'P180D' );
 
 		$response = $controller->index( new Request( ['c' => 'fundraising,fundraising_next'] ) );
 
@@ -45,7 +45,7 @@ class DonationFinishedControllerTest extends TestCase {
 	}
 
 	public function test_given_image_content_type_zero_byte_image_is_returned(): void {
-		$controller = new DonationFinishedController( 'P180D' );
+		$controller = new TestCookieController( 'P180D' );
 
 		$request = new Request( ['c' => 'fundraising,fundraising_next'] );
 		$request->headers->set( 'accept', 'image/gif,image/png,image/*' );
