fd847101b9aba259dc6cfc614f55598db0abc0e7
Abban Dunne <abban.dunne@wikimedia.de>
2022-03-25 16:04:44 +0100

    Update tests that broke from new Symfony version
    Ticket: https://phabricator.wikimedia.org/T300388

--- tests/Unit/EventListener/ExceptionListenerTest.php
+++ tests/Unit/EventListener/ExceptionListenerTest.php
@@ -6,81 +6,49 @@ namespace WMDE\BannerServer\Tests\Unit\EventListener;
 
 use Monolog\Handler\TestHandler;
 use Monolog\Logger;
-use PHPUnit\Framework\Constraint\Callback;
-use PHPUnit\Framework\MockObject\MockObject;
+use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
+use Symfony\Component\EventDispatcher\EventDispatcher;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\Event\ExceptionEvent;
+use Symfony\Component\HttpKernel\HttpKernelInterface;
 use WMDE\BannerServer\EventListener\ExceptionListener;
 
 /**
  * @covers \WMDE\BannerServer\EventListener\ExceptionListener
  */
-class ExceptionListenerTest extends \PHPUnit\Framework\TestCase {
+class ExceptionListenerTest extends KernelTestCase {
+
+	private EventDispatcher $dispatcher;
+
+	public function setUp(): void {
+		$this->dispatcher = new EventDispatcher();
+	}
 
 	public function testGivenExceptionForJavaScriptUrl_thenEmptyInternalServerErrorMessageIsReturned(): void {
-		$testHandler = new TestHandler();
+		$this->withExceptionListener( new TestHandler() );
 
-		$event = $this->newGetResponseForExceptionEventMock(
-			$this->newRequestMock( 'some/url/which/implies/javascript.js' )
-		);
+		$event = $this->makeDispatchedExceptionEvent( 'some/url/which/implies/javascript.js' );
 
-		$callback = new Callback(
-			static function ( $response ): bool {
-				return $response->getContent() === '' &&
-					$response->getStatusCode() === Response::HTTP_INTERNAL_SERVER_ERROR;
-			}
-		);
-
-		$event->expects( $this->once() )
-			->method( 'setResponse' )
-			->with( $callback );
-
-		$listener = new ExceptionListener( new Logger( 'TestLogger', [ $testHandler ] ) );
-		$listener->onKernelException( $event );
+		$this->assertEquals( 'application/javascript; charset=UTF-8', $event->getResponse()->headers->get( 'Content-Type' ) );
+		$this->assertEquals( Response::HTTP_INTERNAL_SERVER_ERROR, $event->getResponse()->getStatusCode() );
 	}
 
 	public function testGivenExceptionForNonJavaScriptUrl_thenEmptyNotFoundErrorMessageIsReturned(): void {
-		$testHandler = new TestHandler();
+		$this->withExceptionListener( new TestHandler() );
 
-		$event = $this->newGetResponseForExceptionEventMock(
-			$this->newRequestMock( 'some/url/which/implies/not/javascript.html' )
-		);
+		$event = $this->makeDispatchedExceptionEvent( 'some/url/which/implies/not/javascript.html' );
 
-		$event->expects( $this->never() )->method( 'setResponse' );
-
-		$listener = new ExceptionListener( new Logger( 'TestLogger', [ $testHandler ] ) );
-		$listener->onKernelException( $event );
+		$this->assertNull( $event->getResponse() );
 	}
 
 	public function testGivenExceptionForJavaScriptUrl_thenErrorsAreLogged(): void {
 		$testHandler = new TestHandler();
-		$response = $this->newGetResponseForExceptionEventMock(
-			$this->newRequestMock( 'some/url/which/implies/javascript.js' )
-		);
+		$this->withExceptionListener( $testHandler );
 
-		$listener = new ExceptionListener( new Logger( 'TestLogger', [ $testHandler ] ) );
-		$listener->onKernelException( $response );
+		$event = $this->makeDispatchedExceptionEvent( 'some/url/which/implies/javascript.js' );
 
 		$this->assertTrue( $testHandler->hasCriticalRecords() );
-	}
-
-	/**
-	 * @param Request $request
-	 * @return ExceptionEvent|MockObject
-	 */
-	private function newGetResponseForExceptionEventMock( Request $request ): ExceptionEvent {
-		$response = $this->createMock( ExceptionEvent::class );
-
-		$response->expects( $this->once() )
-			->method( 'getRequest' )
-			->will( $this->returnValue( $request ) );
-
-		$response->expects( $this->exactly( 2 ) )
-			->method( 'getThrowable' )
-			->will( $this->returnValue( new \Exception( '❌❌❌ Fatal Error: Not enough emojis used. ❌❌❌' ) ) );
-
-		return $response;
 	}
 
 	private function newRequestMock( string $requestUrl ): Request {
@@ -89,6 +57,22 @@ class ExceptionListenerTest extends \PHPUnit\Framework\TestCase {
 			->method( 'getPathInfo' )
 			->will( $this->returnValue( $requestUrl ) );
 		return $request;
+	}
+
+	private function withExceptionListener( TestHandler $testHandler ): void {
+		$listener = new ExceptionListener( new Logger( 'TestLogger', [ $testHandler ] ) );
+		$this->dispatcher->addListener( 'onKernelException', [ $listener, 'onKernelException' ] );
+	}
+
+	private function makeDispatchedExceptionEvent( string $url ): ExceptionEvent {
+		$event = new ExceptionEvent(
+			self::bootKernel(),
+			$this->newRequestMock( $url ),
+			HttpKernelInterface::MAIN_REQUEST,
+			new \Exception( 'This is the error message' )
+		);
+		$this->dispatcher->dispatch( $event, 'onKernelException' );
+		return $event;
 	}
 
 }
